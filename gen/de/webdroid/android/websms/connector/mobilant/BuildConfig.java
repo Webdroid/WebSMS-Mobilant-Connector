5371acb243b2cd856fc88a5383254ca9d6b60a65
Webdroid <webdroid@kabelmail.de>
2012-10-01 21:42:14 +0200

    undo

---
+++ gen/de/webdroid/android/websms/connector/mobilant/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package de.webdroid.android.websms.connector.mobilant;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
